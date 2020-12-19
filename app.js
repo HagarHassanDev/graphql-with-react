f575d208fa28d7502f51dcca976e93d42ffb4081
hager-s3-at-151777343706 <hageraboyabes@gmail.com>
2020-12-19 02:09:35 +0200

    adding buil app.js

--- app.js
+++ app.js
@@ -23,6 +23,14 @@ app.use('/graphql', graphqlHTTP({
 
 const PORT = process.env.PORT || 5001;
 
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, './client/build/index.html'))
+    }
+
 app.listen(PORT, () => {
     console.log('server is listening on port 5000.... ')
 })
